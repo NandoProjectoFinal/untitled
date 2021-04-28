b67e7f4d3000c69e20272347ca9308df36029fff
NandoProjectoFinal <61599328+NandoProjectoFinal@users.noreply.github.com>
2021-04-27 22:16:04 -0400

    Blablabla

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import static java.awt.SystemColor.menu;
+
+public class Main {
+
+    public static void main(String[] args) {
+	menu();
+	
+    }
+
+    private static void menu() {
+
+    }
+}
